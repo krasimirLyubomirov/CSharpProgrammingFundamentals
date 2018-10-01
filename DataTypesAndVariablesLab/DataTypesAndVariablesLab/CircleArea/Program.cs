dd4881be6218d02656402103913aac853bfa743b
Krasimir Lyubomirov <40770219+krasimirLyubomirov@users.noreply.github.com>
2018-10-01 14:21:42 +0300

    Update DataTypesAndVariablesLAb

---
+++ DataTypesAndVariablesLab/DataTypesAndVariablesLab/CircleArea/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CircleArea
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double circleArea = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"{circleArea:f12}");
+        }
+    }
+}
---
+++ DataTypesAndVariablesLab/DataTypesAndVariablesLab/ConcatNames/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConcatNames
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delimiter}{secondName}");
+        }
+    }
+}
